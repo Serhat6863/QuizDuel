56b06405f12ff40c2e908f88f1d136738abff1c3
Serhat6863 <serhatkurklu@icloud.com>
2025-09-28 19:23:18 +0200

    feat(main): add MultiProvider and BlocProviders for auth and room

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 import 'package:quizduel/features/auth/data/repository/user_repository_impl.dart';
 import 'package:quizduel/features/auth/data/service/firebase_user_service.dart';
 import 'package:quizduel/features/auth/domain/repository/user_repository.dart';
@@ -9,8 +10,12 @@ import 'package:quizduel/features/auth/presentation/bloc/auth_event.dart';
 import 'package:quizduel/features/auth/presentation/bloc/login_bloc.dart';
 import 'package:quizduel/features/auth/presentation/bloc/register_bloc.dart';
 import 'package:quizduel/features/auth/presentation/screen/login_screen.dart';
+import 'package:quizduel/features/room/data/repository/room_repository_impl.dart';
+import 'package:quizduel/features/room/domain/repository/room_repository.dart';
+import 'package:quizduel/features/room/presentation/bloc/room_bloc.dart';
 import 'package:quizduel/features/room/presentation/screen/room_home_screen.dart';
 import 'features/auth/presentation/bloc/auth_state.dart';
+import 'features/room/data/service/firebase_room_service.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -18,45 +23,63 @@ void main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   final firebaseUserService = FirebaseUserService();
+  final firebaseRoomService = FirebaseRoomService();
 
   final userRepository = UserRepositoryImpl(
     firebaseUserService: firebaseUserService,
   );
 
-  runApp(MyApp(userRepository: userRepository));
+  final roomRepository = RoomRepositoryImpl(
+    firebaseRoomService: firebaseRoomService,
+  );
+
+  runApp(MyApp(userRepository: userRepository, roomRepository: roomRepository));
 }
 
 class MyApp extends StatelessWidget {
   final UserRepository userRepository;
+  final RoomRepository roomRepository;
 
-  const MyApp({super.key, required this.userRepository});
+  const MyApp({
+    super.key,
+    required this.userRepository,
+    required this.roomRepository,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
+    return MultiProvider(
       providers: [
+
+        Provider<UserRepository>.value(value: userRepository),
+        Provider<RoomRepository>.value(value: roomRepository),
+
+
         BlocProvider<LoginBloc>(
-          create: (_) => LoginBloc(userRepository : userRepository),
+          create: (context) => LoginBloc(userRepository: context.read<UserRepository>()),
         ),
         BlocProvider<RegisterBloc>(
-          create: (_) => RegisterBloc(userRepository: userRepository),
+          create: (context) => RegisterBloc(userRepository: context.read<UserRepository>()),
         ),
         BlocProvider<AuthBloc>(
-          create: (_) => AuthBloc(userRepository: userRepository)..add(AppStarted()),
-        )
+          create: (context) => AuthBloc(userRepository: context.read<UserRepository>())..add(AppStarted()),
+        ),
+        BlocProvider<RoomBloc>(
+          create: (context) => RoomBloc(roomRepository: context.read<RoomRepository>()),
+        ),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         home: BlocBuilder<AuthBloc, AuthState>(
-          builder: (context, state){
-            if(state.status.isLoading){
+          builder: (context, state) {
+            if (state.status.isLoading) {
               return const Scaffold(
                 body: Center(child: CircularProgressIndicator()),
               );
-            }else if(state.status.isAuthenticated){
+            } else if (state.status.isAuthenticated) {
               return const RoomHomeScreen();
-            }else if(state.status.isUnauthenticated) {
+            } else if (state.status.isUnauthenticated) {
               return const LoginScreen();
             }
 
@@ -64,7 +87,7 @@ class MyApp extends StatelessWidget {
               body: Center(child: Text('Something went wrong!')),
             );
           },
-        )
+        ),
       ),
     );
   }
